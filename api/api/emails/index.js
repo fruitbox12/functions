724806845175de02dcfc59823fb0e6f95e1c3c39
fruitbox12 <dylanwong007@gmail.com>
2023-05-16 12:59:16 -0800

    Update index.js

--- api/api/emails/index.js
+++ api/api/emails/index.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const axios = require('axios');
-const qs = require('qs');
+module.exports = (req, res) => {
+  // Mocked email messages
+  const emailMessages = [
+    {
+      id: '1',
+      subject: 'GMAIL API',
+      sender: 'sgrady@innerexplorer.org',
+      body: '72778325262-k4767u04tcv95g7v55ef9fvslc90dnmg.apps.googleusercontent.com'
+    },
+    {
+      id: '2',
+      subject: 'Gmail API Reference',
+      sender: 'sgrady@innerexplorer.org',
+      body: 'https://developers.google.com/gmail/api/reference/rest'
+    }
+  ];
 
-const app = express();
-// Mocked data
-const messages = [
-  { id: '1', subject: 'GMAIL API', sender: 'sgrady@innerexplorer.org', body: '72778325262-k4767u04tcv95g7v55ef9fvslc90dnmg.apps.googleusercontent.com' },
-  { id: '2', subject: 'Gmail API Reference', sender: 'sgrady@innerexplorer.org', body: 'https://developers.google.com/gmail/api/reference/rest' }
-];
-
-app.get('/api/emails', (req, res) => {
-  const email = 'sgrady@innerexplorer.org';
-
-  // Filter messages for the specified email address
-  const filteredMessages = messages.filter(message => message.sender === email);
-
-  res.json(filteredMessages);
-});
-
-module.exports = app;
+  res.status(200).json(emailMessages);
+};
