3e954654a6b557e87dd421dc79446dcc844264fe
fruitbox12 <dylanwong007@gmail.com>
2023-05-22 17:33:19 -0700

    Update index.js

--- api/api/spotify/index.js
+++ api/api/spotify/index.js
@@ -1,6 +1,4 @@
-const express = require('express');
 const fetch = require('node-fetch');
-const app = express();
 
 const token = 'BQANfKbKj1FmSUosZ-w4OkLSfqesTm2T6WNkZIE_bc2-18fq9ABcI1Skon7-MjS5kzcJRCdodP-pA_GGPGN29Ow6J51ezTrhin8lOikKmNzf-suVrME';
 
@@ -10,7 +8,7 @@ async function fetchWebApi(endpoint, method, body) {
       Authorization: `Bearer ${token}`,
     },
     method: method,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
   return await res.json();
 }
@@ -19,13 +17,15 @@ async function getTopTracks() {
   return (await fetchWebApi('v1/me/top/tracks?time_range=short_term&limit=5', 'GET')).items;
 }
 
-app.get('/api/top-tracks', async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const topTracks = await getTopTracks();
-    res.json(topTracks.map(({ name, artists }) => `${name} by ${artists.map(artist => artist.name).join(', ')}`));
+    res.status(200).json(
+      topTracks.map(({ name, artists }) =>
+        `${name} by ${artists.map((artist) => artist.name).join(', ')}`
+      )
+    );
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch top tracks' });
   }
-});
-
-module.exports = app;
+};
